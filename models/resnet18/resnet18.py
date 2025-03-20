84dc94731e244355cc291289be71f56a8f54e1e6
Andrii Norets <104683293+AndriiNorets@users.noreply.github.com>
2025-03-20 19:31:54 +0100

    MODEL | ResNet18
    * ResNet18 model architecture
    
    * Datamodule tests, change batch size in datamodule
    
    * Update bump version
    
    ---------
    
    Co-authored-by: Andrii Norets <104683293+KabinetFnsv@users.noreply.github.com>

---
+++ models/resnet18/resnet18.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+from pytorch_lightning import LightningModule
+
+
+class PneumoniaResNet(LightningModule):
+    def __init__(self, num_classes=2, learning_rate=1e-3):
+        super().__init__()
+        self.model = models.resnet18(pretrained=True)
+        self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
+        self.learning_rate = learning_rate
+        self.criterion = nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        self.log("val_loss", loss)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
